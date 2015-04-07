37ed7456326cd17577fa8b23ad012f4a87c2edf4
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-04-07 14:03:00 -0700

    Add basic users and auth

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'newrelic_rpm'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
@@ -36,6 +36,8 @@ gem 'newrelic_rpm'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,38 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :login_user!, :logout_user!
+  
+  private
+  def require_logged_in
+    unless logged_in?
+      redirect_to new_session_url
+    end
+  end
+
+  def require_logged_out
+    if logged_in?
+      redirect_to root_url
+    end
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login_user!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout_user!
+    @current_user.reset_session_token!
+    @current_user = nil
+    session[:session_token] = nil
+  end
 end
