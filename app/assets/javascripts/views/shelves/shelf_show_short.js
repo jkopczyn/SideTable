02e453697a68d549efb837b908fe18382fa6f277
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-04-10 13:32:04 -0700

    Fill out the searchviews

--- app/assets/javascripts/views/shelves/shelf_show_short.js
+++ app/assets/javascripts/views/shelves/shelf_show_short.js
@@ -3,23 +3,15 @@ SideTable.Views.ShelfShowShort = Backbone.CompositeView.extend({
   template: JST['shelves/show_short'],
 
   initialize: function(options) {
-    this.listenTo(this.model, "sync change:title", this.render);
-    this.listenTo(this.model.games(), "remove sync", this.render);
-    this.listenTo(this.model.games(), "add", this.addGame);
+    this.itemView = SideTable.Views.GameItemShort;
+    this.selector = ".shelf-show-short";
+    this.collection = this.model.games();
 
-    this.model.games().each(this.addGame);
+    SideTable.Views.GameSearchView.prototype.initialize.apply(this,arguments);
   },
 
   render: function() {
-    this.$el.html(this.template({
-      shelf: this.model 
-    }));
-    this.attachSubviews();
-    return this;
-  },
-
-  addGame: function(game) {
-      var view = new SideTable.Views.GameItemShort({ model: game });
-      this.addSubview('.shelf-show-short', view);
+    return SideTable.Views.GameSearchView.prototype.render.call(
+      this, { shelf: this.model })
   },
 });
