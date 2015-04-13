4718b20efeca433b24854aceefcc634f98d602bb
jkopczyn <ja.kopczynski@gmail.com>
2015-04-13 14:39:12 -0700

    Add inverse_of to fix validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,10 @@
 class Game < ActiveRecord::Base
   validates :title, presence: true
-  has_many :shelvings
+  has_many :shelvings, inverse_of: :game
   has_many :shelves, through: :shelvings
   has_many :users, through: :shelves
-  has_many :ratings
-  has_many :reviews
+  has_many :ratings, inverse_of: :game
+  has_many :reviews, inverse_of: :game
   #has_many :taggings
   #has_many :tags, through: :taggings
 end
