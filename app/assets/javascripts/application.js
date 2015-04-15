411e81530e9f1a23c72bfd1393e377b580e0baeb
jkopczyn <ja.kopczynski@gmail.com>
2015-04-14 17:48:53 -0700

    Modify manifests and rating validations for rateYo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery.serializejson
+//= require jquery.rateyo
 //= require jquery_ujs
 //= require underscore
 //= require backbone
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,4 +5,6 @@ class Rating < ActiveRecord::Base
   validates :user, :game, :score, presence: true
   validates :user, uniqueness: {scope: :game}
   validates :game, uniqueness: {scope: :user}
+  validates :score, :inclusion: { in: (1..50) }
+  #stores a five-star rating with decimals; 1 => 0.1, 50 => 5.0
 end
