87439f9af4398fb58363a8b63457e69d22710f66
jkopczyn <ja.kopczynski@gmail.com>
2015-04-15 13:19:35 -0700

    Restore ability to add games

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -2,6 +2,6 @@ class Shelf < ActiveRecord::Base
   validates :user_id, :title, presence: true
 
   has_many :shelvings
-  has_many :games, through: :shelvings
+  has_many :games, through: :shelvings, source: :game
   belongs_to :user
 end
